acc299812c7e3a9ff45e1bfc2194279e5b6237c2
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-12-10 10:06:44 +0300

    Initial Commit

---
+++ AnimationAndGestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationAndGestures
+//
+//  Created by Nikolay Shubenkov on 10/12/2016.
+//  Copyright © 2016 Nikolay Shubenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
