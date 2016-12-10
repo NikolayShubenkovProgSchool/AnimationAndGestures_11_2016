3e3a7a611cc7dfb034427fc51a998871f5acab32
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-12-10 16:56:33 +0300

    roation works

--- AnimationAndGestures/HandleStaloneViewController.swift
+++ AnimationAndGestures/HandleStaloneViewController.swift
@@ -10,16 +10,69 @@ import UIKit
 
 class HandleStaloneViewController: UIViewController {
 
+    @IBOutlet var stalone: UIImageView!
+    @IBOutlet var staloneWidth: NSLayoutConstraint!
     @IBOutlet var topSpaceToStalone: NSLayoutConstraint!
     
     @IBOutlet var leftSpaceToStalone: NSLayoutConstraint!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRotateGesture()
+    }
+    
+    private func setupRotateGesture()
+    {
+        let rotation = UIRotationGestureRecognizer(target: self,
+                                                   action: #selector(HandleStaloneViewController.rotateGesture(_:)))
+        
+        stalone.addGestureRecognizer(rotation)
+    }
+    
+    //MARK: - UI Events
+    
+    func rotateGesture(_ gesture:UIRotationGestureRecognizer)
+    {
+        UIView.animate(withDuration: 0.1,
+                       animations:{
+                        switch gesture.state
+                        {
+                        case .ended, .changed:
+                            self.stalone.transform = self.stalone.transform.rotated(by: gesture.rotation)
+                            gesture.rotation = 0
+                            
+                        default:
+                            break
+                        }
+        })
+    }
+    
     @IBAction func changeBackgroundGesture(_ sender: UITapGestureRecognizer)
     {
         UIView.animate(withDuration: 0.3, animations: {
             self.view.backgroundColor = UIColor.randColor()
         })
     }
+    
+    @IBAction func scaleGesture(_ gesture: UIPinchGestureRecognizer)
+    {
+        switch gesture.state
+        {
+        case .ended,.changed:
+            let scale = gesture.scale
+            
+            UIView.animate(withDuration: 0.1, animations: { 
+                self.staloneWidth.constant *= scale
+                self.view.layoutIfNeeded()
+            })
+            gesture.scale = 1
+            
+        default:
+            break
+        }
+    }
+    
+    
     
     @IBAction func translationGesture(_ gesture: UIPanGestureRecognizer)
     {
